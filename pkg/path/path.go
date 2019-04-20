20f67c216085810e30650b729ad4dc77e85793c0
Adam Houston <phazyy9@gmail.com>
2019-04-20 23:20:41 +0100

    Simplfying path logic

--- pkg/path/path.go
+++ pkg/path/path.go
@@ -10,52 +10,19 @@ import (
 
 // CurrentDirectory returns the current working directory.
 func CurrentDirectory(cwd string) string {
+	return color.Sprintf(color.Blue, sanitiseDirectory(cwd))
+}
+
+func sanitiseDirectory(cwd string) string {
 	if cwd == "/" {
-		return color.Sprintf(color.Blue, cwd)
+		return cwd
 	}
 
-	nearestAccessiblePath := findNearestAccessiblePath(cwd)
-	if nearestAccessiblePath != cwd {
-		inAccessiblePath := strings.TrimPrefix(cwd, nearestAccessiblePath)
-		nearestAccessiblePath = shortenLongPath(stripHomeDir(nearestAccessiblePath), 1)
-
-		return color.Sprintf(color.Blue, nearestAccessiblePath) +
-			color.Sprintf(color.Red, inAccessiblePath)
+	if info, err := os.Stat(cwd); !info.IsDir() && err == nil {
+		cwd = filepath.Dir(cwd)
+	} else if err != nil {
+		return cwd
 	}
 
-	pathToDisplay := stripHomeDir(cwd)
-	pathToDisplay = shortenLongPath(pathToDisplay, 2)
-
-	return color.Sprintf(color.Blue, pathToDisplay)
-}
-
-func findNearestAccessiblePath(path string) string {
-	_, err := os.Stat(path)
-	if err == nil {
-		return path
-	}
-
-	return findNearestAccessiblePath(filepath.Dir(path))
-}
-
-func shortenLongPath(path string, length int) string {
-	pList := strings.Split(path, "/")
-	if len(pList) < 7 {
-		return path
-	}
-
-	shortenedPList := pList[:len(pList)-length]
-	for i, v := range shortenedPList {
-		// shortenedPList[0] will be an empty string due to leading '/'
-		if len(v) > 0 {
-			shortenedPList[i] = v[:1]
-		}
-	}
-
-	shortenedPList = append(shortenedPList, pList[len(pList)-length:]...)
-	return strings.Join(shortenedPList, "/")
-}
-
-func stripHomeDir(path string) string {
-	return strings.Replace(path, os.Getenv("HOME"), "~", 1)
+	return strings.Replace(cwd, os.Getenv("HOME"), "~", 1)
 }
